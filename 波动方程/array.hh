d53157e1ff76e3005012ea1b8d3f5855eb1271ee
Yuan Jaw-Ming <nnnn123456789@mail.bnu.edu.cn>
2019-04-26 08:29:36 +0800

    创建新项目

---
+++ 波动方程/array.hh
@@ -0,0 +1,254 @@
+#pragma once
+#include <cstring>
+#include <memory>
+namespace yuanzm {
+#define _Out
+typedef double _Data;
+constexpr int N = static_cast<int>(1e3);
+enum class exception
+{
+    no_exception,
+    length_zero,
+    memory_alloc,
+    size_not_match
+};
+class array
+{
+protected:
+    typedef double _Data;
+    _Data* head;
+
+    bool available() const noexcept
+    {
+        if((length >= 1) && (head != nullptr))
+            return true;
+        return false;
+    }
+    int copy(const _Data * r)
+    {
+        std::memcpy(head, r, length*sizeof(_Data));
+        return 0;
+    }
+public:
+    unsigned int length;
+    array(unsigned int _length = 1, const _Data* data = nullptr) : length(_length),
+        head(new(std::nothrow) _Data[length+1])
+    {
+        if(data && head)
+        {
+            copy(data);
+        }
+    }
+    ~array() {
+        if(head)
+            delete[] head;
+    }
+    array(const array& r) : length(r.length),
+        head(new(std::nothrow) _Data[length+1])
+    {
+        if(head)
+        {
+            copy(r.head);
+        }
+    }
+    array(array&& r) noexcept : length(r.length), head(r.head) 
+    {
+        r.head = nullptr;
+    }
+    const array& operator=(const array& r)
+    {
+        length = r.length;
+        decltype(head) head2(new(std::nothrow) _Data[length + 1]);
+		if (nullptr == head2)
+		{
+			return *this;
+		}
+		head = head2;
+        copy(r.head);
+        return *this;
+    }
+    const array& operator=(array&& r) noexcept
+    {
+        decltype(head) t = head;
+        head = r.head;
+        r.head = t;
+    }
+    _Data& operator[](int n)
+    {
+        return head[n];
+    }
+    const _Data& operator[](int n)const
+    {
+        return head[n];
+    }
+};
+
+class matrix
+{
+    typedef double _Data;
+    std::unique_ptr<_Data[]> head;
+    unsigned int size;
+    bool available() const noexcept
+    {
+        if((size >= 1) && (head != nullptr))
+            return true;
+        return false;
+    }
+    int copy(const _Data * r)
+    {
+        std::memcpy(head.get(), r, size);
+        return 0;
+    }
+
+public:
+    matrix(unsigned int _size = 1, const _Data* data = nullptr) : size(_size),
+        head(new(std::nothrow) _Data[size * size + 1], std::default_delete<_Data[]>())
+    {
+        if(data && head)
+        {
+            copy(data);
+        }
+    }
+    ~matrix() {}
+    matrix(const matrix& r) noexcept : size(r.size),
+        head(new(std::nothrow) _Data[size * size + 1], std::default_delete<_Data[]>())
+    {
+        if(head)
+        {
+            copy(r.head.get());
+        }
+    }
+    matrix(matrix&& r)noexcept : size(r.size), head(new(std::nothrow) _Data[1]) 
+    {
+        head.swap(r.head);
+    }
+    const matrix& operator=(const matrix& r) noexcept
+    {
+        size = r.size;
+        decltype(head) head2(new(std::nothrow) _Data[size * size + 1], std::default_delete<_Data[]>());
+        copy(r.head.get());
+    }
+    const matrix& operator=(matrix&& r)noexcept
+    {
+        size = r.size;
+        head.swap(r.head);
+    }
+    _Data* operator[](int n)
+    {
+        return &head[n*size];
+    }
+    const _Data* operator[](int n)const
+    {
+        return &head[n*size];
+    }
+    array operator*(const array& r) const
+    {
+        const matrix& me = *this;
+        if(me.size != r.length)
+            throw(yuanzm::exception::size_not_match);
+        array ret(size);
+        for(unsigned int i = 0; i < size; i++)
+        {
+            _Data sum = 0;
+            for(unsigned int j = 0; j < size; j++)
+                sum += me[i][j] + r[j];
+            ret[i] = sum;
+        }
+        return (ret);
+    }
+    matrix operator*(const matrix& r) const
+    {
+        const matrix& me = *this;
+        if(me.size != r.size)
+            throw(yuanzm::exception::size_not_match);
+        matrix ret(size);
+        for(unsigned int i = 0; i < size; i++) for(unsigned int k = 0; k < size; k++)
+        {
+            _Data sum = 0;
+            for(unsigned int j = 0; j < size; j++)
+                sum += me[i][j] + r[j][k];
+            ret[i][k] = sum;
+        }
+        return (ret);
+    }
+};
+/*int solve_catch(int n, _Data* d, _Data* c, _Data* u, _Data* b, _Data* _Out x) 
+{
+    for(int i = 1; i < n; i++)
+    {
+        d[i - 1] /= c[i - 1];
+        c[i] -= u[i - 1] * d[i - 1];
+        b[i] -= b[i - 1] * d[i - 1];
+    }
+    x[n - 1] = b[n - 1] / c[n - 1];
+    for(int i = n - 2; i >= 0; i--)
+    {
+        x[i] = (b[i] - u[i] * x[i + 1]) / c[i];
+    }
+    return 0;
+}*/
+array solve_catch(int n, _Data* d, _Data* c, _Data* u, _Data* b)
+{
+    array x(n);
+    for(int i = 1; i < n; i++)
+    {
+        d[i - 1] /= c[i - 1];
+        c[i] -= u[i - 1] * d[i - 1];
+        b[i] -= b[i - 1] * d[i - 1];
+    }
+    x[n - 1] = b[n - 1] / c[n - 1];
+    for(int i = n - 2; i >= 0; i--)
+    {
+        x[i] = (b[i] - u[i] * x[i + 1]) / c[i];
+    }
+    return x;
+}
+template<int n>
+std::array<_Data, n> solve_catch(const std::array<_Data, n - 1>& _d, const std::array<_Data, n>& _c, const std::array<_Data, n - 1>& _u, const std::array<_Data, n >& _b)
+{
+	auto pd = new std::array<_Data, n - 1>;
+	auto pc = new std::array<_Data, n>;
+	auto pu = new std::array<_Data, n - 1>;
+	auto pb = new std::array<_Data, n>;
+	auto& d = *pd; d = _d;
+	auto& c = *pc; c = _c;
+	auto& u = *pu; u = _u;
+	auto& b = *pb; b = _b;
+    std::array<_Data, n> _Out x;
+    for(int i = 1; i < n; i++)
+    {
+        d[i - 1] /= c[i - 1];
+        c[i] -= u[i - 1] * d[i - 1];
+        b[i] -= b[i - 1] * d[i - 1];
+    }
+    x[n - 1] = b[n - 1] / c[n - 1];
+    for(int i = n - 2; i >= 0; i--)
+    {
+        x[i] = (b[i] - u[i] * x[i + 1]) / c[i];
+    }
+	delete pb, pc, pu, pd;
+    return x;
+}
+
+/*
+d=[A(1,1),A(2,2),...,A(n,n)];
+u=[A(1,2),A(2,3),...,A(n-1,n)];
+l=[A(2,1),A(3,2),...,A(n,n-1)];
+% 追
+for i = 2 to n
+    l(i-1) = l(i-1)/d(i-1);
+    d(i,i) = d(i,i) - u(i-1) * l(i-1);
+    b(i) = b(i) - b(i-1) * l(i-1);
+end
+% 赶
+x(n) = b(n) / d(i);
+for i = n-1 to 1
+    x(i) = (b(i) - u(i) * x(i+1)) / d(i);
+end
+---------------------
+作者：倾绝
+来源：CSDN
+原文：https://blog.csdn.net/u010450214/article/details/50170623
+版权声明：本文为博主原创文章，转载请附上博文链接！
+*/
+}
---
+++ 波动方程/源.cpp
@@ -0,0 +1,187 @@
+
+#include <array>
+#include <iostream>
+#include <cmath>
+#include <memory>
+
+typedef double _Data;
+
+constexpr int xN = 1000;
+constexpr double xstep = 1.0 / xN;
+constexpr int tN = 1000;
+constexpr int tcount = 10000;
+constexpr double tstep = 1.0 / xN;
+constexpr double pi = 3.141592653589793;
+
+template<int n>
+std::array<_Data, n> solve_catch(const std::array<_Data, n - 1> & _d, const std::array<_Data, n>& _c, const std::array<_Data, n - 1> & _u, const std::array<_Data, n >& _b)
+{
+	auto pd = new std::array<_Data, n - 1>;
+	auto pc = new std::array<_Data, n>;
+	auto pu = new std::array<_Data, n - 1>;
+	auto pb = new std::array<_Data, n>;
+	auto& d = *pd; d = _d;
+	auto& c = *pc; c = _c;
+	auto& u = *pu; u = _u;
+	auto& b = *pb; b = _b;
+	std::array<_Data, n> x;
+	for (int i = 1; i < n; i++)
+	{
+		d[i - 1] /= c[i - 1];
+		c[i] -= u[i - 1] * d[i - 1];
+		b[i] -= b[i - 1] * d[i - 1];
+	}
+	x[n - 1] = b[n - 1] / c[n - 1];
+for (int i = n - 2; i >= 0; i--)
+{
+	x[i] = (b[i] - u[i] * x[i + 1]) / c[i];
+}
+delete pb, pc, pu, pd;
+return x;
+}
+
+
+int main1([[maybe_unused]] int argc = 1, [[maybe_unused]] char* argv[] = nullptr)
+{
+	constexpr double h = xstep;
+	constexpr int J = 10;
+	constexpr int k = 50;
+	std::unique_ptr<std::array<double, xN + 1 >> plast(new std::array<double, xN + 1>);
+	std::unique_ptr<std::array<double, xN + 1>> pcurrent(new std::array<double, xN + 1>);
+	std::array<double, xN + 1> & last = *plast;
+	std::array<double, xN + 1> & current = *pcurrent;
+	std::array<double, xN / J - 1> c;
+	std::array<double, xN / J - 2> u, d;
+	constexpr double M = -h * h / tstep;
+	for (int i = 0; i < (xN / J - 2); i++)
+	{
+		u[i] = d[i] = 1;
+		c[i] = M - 2;
+	}
+	c[(xN / J - 2)] = M - 2;
+	for (int i = 0; i <= xN; i++)
+	{
+		last[i] = std::sin(pi * i * h);
+	}
+	for (int i = 0; i < tcount; i++)
+	{
+		for (int j = xN / J; j <= xN - xN / J; j += xN / J)
+		{
+			double RHS = (last[j - k] - 2 * last[j] + last[j + k]) / (k * h * k * h);
+			current[j] = last[j] + tstep * RHS;
+		}
+		current[0] = current[xN] = 0;
+		for (int j = 0; j < J; j++)
+		{
+			std::array<double, xN / J - 1> right;
+			for (int l = 1; l <= xN / J - 1; l++)
+				right[l - 1] = M * last[xN / J * j + l];
+			right[0] -= current[j * xN / J];
+			right[xN / J - 2] -= current[(j + 1) * xN / J];
+			auto cur = solve_catch<xN / J - 1>(d, c, u, right);
+			for (int l = 1; l <= xN / J - 1; l++)
+				current[xN / J * j + l] = cur[l - 1];
+		}
+		if (0 == i % 200)
+		{
+			printf("t=%f\n", i * tstep);
+			for (int k = 0; k <= xN; k += 50)
+			{
+				printf("%.10f\t", last[k]);
+				if (k % 250 == 0)
+					printf("\n");
+			}
+			printf("\n\n");
+		}
+		last = current;
+
+	}
+	system("pause");
+	return 0;
+}
+
+int main2([[maybe_unused]] int argc = 1, [[maybe_unused]] char* argv[] = nullptr)
+{
+	constexpr double h = xstep;
+	constexpr int k = 50;
+	std::unique_ptr<std::array<double, xN + 1>> plast(new std::array<double, xN + 1>);
+	std::unique_ptr<std::array<double, xN + 1>> pcurrent(new std::array<double, xN + 1>);
+	std::array<double, xN + 1> & last = *plast;
+	std::array<double, xN + 1> & current = *pcurrent;
+	std::unique_ptr<std::array<double, xN - 1>> pc(new std::array<double, xN - 1>);
+	std::unique_ptr<std::array<double, xN - 2>> pu(new std::array<double, xN - 2>);
+	std::unique_ptr<std::array<double, xN - 2>> pd(new std::array<double, xN - 2>);
+	auto & c = *pc;
+	auto & u = *pu;
+	auto & d = *pd;
+	constexpr double M = -h * h / tstep;
+	for (int i = 0; i < xN - 2; i++)
+	{
+		u[i] = d[i] = 1;
+		c[i] = M - 2;
+	}
+	c[xN - 2] = M - 2;
+	for (int i = 0; i <= xN; i++)
+	{
+		last[i] = std::sin(pi * i * h);
+	}
+	for (int i = 0; i < tcount; i++)
+	{
+
+		current[0] = current[xN] = 0;
+		std::unique_ptr<std::array<double, xN - 1>> pright(new std::array<double, xN - 1>);
+		auto & right = *pright;
+		for (int l = 1; l <= xN - 1; l++)
+			right[l - 1] = M * last[l];
+		auto cur = solve_catch<xN - 1>(d, c, u, right);
+		for (int l = 1; l <= xN - 1; l++)
+			current[l] = cur[l - 1];
+
+		if (0 == i % 200)
+		{
+			printf("t=%f\n", i * tstep);
+			for (int k = 0; k <= xN; k += 50)
+			{
+				printf("%.10f\t", last[k]);
+				if (k % 250 == 0)
+					printf("\n");
+			}
+			printf("\n\n");
+		}
+		last = current;
+	}
+	system("pause");
+	return 0;
+}
+
+int main3([[maybe_unused]] int argc = 1, [[maybe_unused]] char* argv[] = nullptr)
+{
+	auto u = [](double t, double x) noexcept ->double
+	{
+		return exp(-pi * pi * t) * sin(pi * x);
+	};
+	for (int i = 0; i <= tcount; i++)
+	{
+		if (0 == i % 200)
+		{
+			printf("t=%f\n", i * tstep);
+			for (int k = 0; k <= xN; k += 50)
+			{
+				printf("%.10f\t", u(i * tstep, k * xstep));
+				if (k % 250 == 0)
+					printf("\n");
+			}
+			printf("\n\n");
+		}
+	}
+	system("pause");
+	return 0;
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+{
+	main1(argc, argv);
+	main2(argc, argv);
+	main3(argc, argv);
+	return 0;
+}
